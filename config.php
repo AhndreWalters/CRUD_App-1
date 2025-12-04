0661bb5c75220d48906a86f667a6bb2f7ffdcc4f
AhndreWalters <120767274+AhndreWalters@users.noreply.github.com>
2025-12-04 14:42:30 +0000

    Update config.php

--- config.php
+++ config.php
@@ -1,25 +1,17 @@
 <?php
+/* Database credentials. Assuming you are running MySQL
+server with default setting (user 'root' with no password) */
 define('DB_SERVER', 'mysql-640c046-dessyabraham2000-c866.k.aivencloud.com');
-define('DB_USERNAME', 'avnadmin');
-define('DB_PASSWORD', '$2y$10$WsVpM7DyxiXJPb88pp1d2.rc5QlOyXypzrXnhoDfs1DSMZX6mtWCO'); // use the plain password from Aiven dashboard
 define('DB_NAME', 'crud_app');
-define('DB_PORT', 12272);
-
-$link = mysqli_init();
-
-// set SSL with the correct certificate path
-mysqli_ssl_set($link, NULL, NULL, __DIR__ . 'certs/ca.pem', NULL, NULL);
-
-if (!mysqli_real_connect(
-    $link,
-    DB_SERVER,
-    DB_USERNAME,
-    DB_PASSWORD,
-    DB_NAME,
-    DB_PORT,
-    NULL,
-    MYSQLI_CLIENT_SSL
-)) {
+define('DB_USERNAME', 'avnadmin');
+define('DB_PASSWORD', '$2y$10$WsVpM7DyxiXJPb88pp1d2.rc5QlOyXypzrXnhoDfs1DSMZX6mtWCO');
+define('DB_PORT', '12272');
+ 
+/* Attempt to connect to MySQL database */
+$link = mysqli_connect(DB_SERVER, DB_USERNAME, DB_PASSWORD, DB_NAME, DB_PORT);
+ 
+// Check connection
+if($link === false){
     die("ERROR: Could not connect. " . mysqli_connect_error());
 }
 ?>
